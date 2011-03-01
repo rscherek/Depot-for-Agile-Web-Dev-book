948a07495474222f50b91959a83fe531154ff5e8
Phil Goodwin & Richard Scherek <pair+phil+richard@pivotallabs.com>
2011-03-01 13:45:44 -0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Depot::Application.config.secret_token = '490bca08075faf85bbecf24d978da9d4ef21aa7b3f5bc977bc66c68c894ec0d6ea2970daf7959e50194b187078f836035062eee8ef46ad84b02266d521615d80'
